07f94449c0928ec9f2cb7300837e97fd6710a09d
laura <ljarinl@gmail.com>
2020-04-21 23:16:10 -0400

    switch to using polynomials instead of integration. may want integration back for like more complex A/B for non-quadrotor. can copy functions from git history

--- min_dispersion_primitives.py
+++ min_dispersion_primitives.py
@@ -2,12 +2,14 @@
 import numpy as np
 from scipy.linalg import expm
 import scipy.integrate as integrate
+from scipy.special import factorial
 import matplotlib.pyplot as plt
 import cProfile
 from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
 import time
 import pickle
+import sympy as sym
 
 
 class MotionPrimitive():
@@ -30,26 +32,25 @@ class MotionPrimitive():
         self.num_dts = 60  # Number of time horizons to consider between 0 and max_dt
 
         self.A, self.B = self.A_and_B_matrices_quadrotor()
+        self.quad_dynamics_polynomial = self.quad_dynamics_polynomial_symbolic()
+        # self.quad_dynamics_polynomial = self.quad_dynamics_polynomial_symbolic_better_vectorization()
 
     def compute_all_possible_mps(self, start_pt):
 
         single_u_set = np.linspace(-self.max_u, self.max_u, self.num_u_set)
         dt_set = np.linspace(self.min_dt, self.max_dt, self.num_dts)
-        integral_set = np.empty((self.num_dts, self.n, self.num_dims))
-        expm_A_set = np.empty((self.num_dts, self.n, self.n))
-        for i, dt in enumerate(dt_set):
-            integral_set[i, :, :] = integrate.quad_vec(self.quad_dynamics_integral_wrapper(dt), 0, dt)[0]
-            expm_A_set[i, :, :] = expm(self.A*dt)
         u_grid = np.meshgrid(*[single_u_set for i in range(self.num_dims)])
         u_set = np.dstack(([x.flatten() for x in u_grid]))[0].T
+        sample_pts = None
+        for dt_index in range(dt_set.shape[0]):  # TODO vectorize better. Has to be in the symbolic math function (quad_dynamics_polynomial)
+            sp = np.array(self.quad_dynamics_polynomial(start_pt, u_set, dt_set[dt_index]))
+            if sample_pts is None:
+                sample_pts = sp
+            else:
+                sample_pts = np.dstack((sample_pts, sp))
+        # print(self.quad_dynamics_polynomial(start_pt, u_set, dt_set))
+        sample_pts = np.transpose(sample_pts, (1, 2, 0))
 
-        first_terms = expm_A_set@start_pt  # indexed by dt index num_dt x n x 1
-        first_terms_repeated = np.repeat(first_terms, self.num_u_set**self.num_dims,
-                                         2)  # num_dt x n x num_u_set**num_dims
-        second_terms = integral_set@u_set  # num_dt x n x num_u_set^num_dims
-
-        sample_pts = first_terms_repeated + second_terms
-        sample_pts = np.transpose(sample_pts, (0, 2, 1))  # Order so that it's dt_index, du_index, state_space_index
         if self.plot:
             if self.num_dims > 1:
                 plt.plot(sample_pts[:, :, 0], sample_pts[:, :, 1], marker='.', color='k', linestyle='none')
@@ -103,18 +104,9 @@ class MotionPrimitive():
     def create_evenly_spaced_mps(self, start_pt, dt):
         # i.e. old sikang method
         single_u_set = np.linspace(-self.max_u, self.max_u, self.num_u_per_dimension)
-        integral_set = np.empty((self.n, self.num_dims))
-        expm_A_set = np.empty((self.n, self.n))
-        integral_set = integrate.quad_vec(self.quad_dynamics_integral_wrapper(dt), 0, dt)[0]
-        expm_A_set = expm(self.A*dt)
         u_grid = np.meshgrid(*[single_u_set for i in range(self.num_dims)])
         u_set = np.dstack(([x.flatten() for x in u_grid]))[0].T
-
-        first_terms = expm_A_set@start_pt
-        first_terms_repeated = np.repeat(first_terms, self.num_output_mps,
-                                         1)
-        second_terms = integral_set@u_set
-        sample_pts = first_terms_repeated + second_terms
+        sample_pts = np.array(self.quad_dynamics_polynomial(start_pt, u_set, dt))
         if self.plot:
             plt.plot(sample_pts[0, :], sample_pts[1, :], 'oy')
 
@@ -131,13 +123,10 @@ class MotionPrimitive():
 
         prim_list = []
         for start_pt in start_pts_set.T:
-            # TODO store in a cool CS way so that lookup is fast
             prim_list.append(mp.compute_min_dispersion_set(np.reshape(start_pt, (self.n, 1))))
             if self.plot:
                 plt.show()
 
-        # np.save('start_pts.npy', start_pts_set)
-        # np.save('motion_prims.npy', prim_list)
         self.start_pts = start_pts_set
         self.motion_primitives_list = prim_list
         with open('pickle/MotionPrimitive.pkl', 'wb') as output:  # TODO add timestamp of something back
@@ -166,6 +155,39 @@ class MotionPrimitive():
     def quad_dynamics(self, start_pt, u, dt):
         return expm(self.A*dt)@start_pt + integrate.quad_vec(self.quad_dynamics_integral_wrapper(dt), 0, dt)[0]@u
 
+    def quad_dynamics_polynomial_symbolic(self):
+        start_pt = sym.Matrix([sym.symbols('start_pt%d' % i) for i in range(self.n)])
+        u = sym.Matrix([sym.symbols('u%d' % i) for i in range(self.num_dims)])
+        dt = sym.symbols('dt')
+        pos = u*dt**self.control_space_q/factorial(self.control_space_q)
+        for i in range(self.control_space_q):
+            pos += sym.Matrix(start_pt[i*self.num_dims:(i+1)*self.num_dims]) * dt**i/factorial(i)
+        x = pos
+        for j in range(1, self.control_space_q):
+            d = sym.diff(pos, dt, j)
+            x = np.vstack((x, d))
+        x = x.T[0]
+        return sym.lambdify([start_pt, u, dt], x)
+
+    def quad_dynamics_polynomial_symbolic_better_vectorization(self):
+        # not working
+        start_pt = sym.Matrix([sym.symbols('start_pt%d' % i) for i in range(self.n)])
+        u = sym.Matrix([sym.symbols('u%d' % i) for i in range(self.num_dims)])
+        dt = sym.Matrix([sym.symbols('dt')])
+        pos = np.outer(u, dt**(self.control_space_q)) / factorial(self.control_space_q)
+        for i in range(self.control_space_q):
+            pos += np.outer(sym.Matrix(start_pt[i*self.num_dims:(i+1)*self.num_dims]), dt**i)/factorial(i)
+        x = pos
+        x = x.reshape((self.num_dims))
+        for j in range(1, self.control_space_q):
+            d = sym.diff(pos, dt, j)
+            d = d.reshape((self.num_dims))
+            x = np.hstack((x, d))
+        print(x)
+        # x = np.flip(x.T)[0]
+        # x = x.reshape((self.n, 1))
+        return sym.lambdify([start_pt, u, dt], x)
+
 
 if __name__ == "__main__":
     control_space_q = 3
@@ -173,16 +195,20 @@ if __name__ == "__main__":
     num_u_per_dimension = 5
     max_state_derivs = [1, 1, 1, 1]
     num_state_deriv_pts = 5
-    plot = False
+    plot = True
     mp = MotionPrimitive(control_space_q=control_space_q, num_dims=num_dims,
                          num_u_per_dimension=num_u_per_dimension, max_state_derivs=max_state_derivs, num_state_deriv_pts=num_state_deriv_pts, plot=plot)
-    start_pt = np.ones((mp.n, 1))*0.5
-    # mp.compute_all_possible_mps(start_pt)
+    start_pt = np.ones((mp.n))*0.5
+    mp.compute_all_possible_mps(start_pt)
 
     # with PyCallGraph(output=GraphvizOutput()):
 
     # mp.compute_min_dispersion_set(start_pt)
     # mp.create_evenly_spaced_mps(start_pt, mp.max_dt/2.0)
 
-    mp.create_state_space_MP_lookup_table()
+    # mp.create_state_space_MP_lookup_table()
+
+    print(mp.quad_dynamics_polynomial(start_pt,[1,1],1.5))
+    print(mp.quad_dynamics(start_pt,[1,1],1.5))
+
     plt.show()
