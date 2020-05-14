6520d8a6b41156594b1dfe1d0887e27a9014b940
Ray <raybjork@gmail.com>
2020-05-14 19:11:45 -0400

    Octomap listener now reads in a static [map].bt file and creates OcTree, method using octomap_msg does not work

--- scripts/octomap_listener.py
+++ scripts/octomap_listener.py
@@ -1,12 +1,17 @@
+import os
 import rospy
 from octomap_msgs.msg import Octomap
 import octomap
 
 
 def octomap_callback(msg):
-    octree = octomap.OcTree(msg.resolution)
-    octomap.OcTree.read(msg.data)
+    tree = octomap.OcTree(msg.resolution)
 
+def create_OcTree():
+    filename = os.path.join(os.path.dirname(__file__), '../data/fr_078_tidyup.bt')
+    return octomap._octree_read(bytes(filename, encoding='utf-8'))
+
+tree = create_OcTree()
 
 rospy.init_node('octomap_listener', anonymous=True)
 rospy.Subscriber("octomap_full", Octomap, octomap_callback)
