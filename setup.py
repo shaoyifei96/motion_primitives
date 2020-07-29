c1a1926525abc044872d9d73431aceddaa879402
laura <ljarinl@gmail.com>
2020-07-29 17:27:07 -0400

    fix cmakelists python version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='motion_primitives_py', version='1.0', packages=['motion_primitives_py'])
+setup(name='motion_primitives_py', version='0.0.1', packages=['motion_primitives_py'])
