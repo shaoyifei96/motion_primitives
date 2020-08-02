655917e4157da84d6e5ff02d171cd38f856525d3
Ray <raybjork@gmail.com>
2020-08-02 13:18:41 -0400

    implementing margins in occupancy map, ignore rosbag files

--- motion_primitives_py/occupancy_map.py
+++ motion_primitives_py/occupancy_map.py
@@ -1,6 +1,8 @@
 import rosbag
 import numpy as np
 import matplotlib.pyplot as plt
+
+# take these out later
 from motion_primitives_py.polynomial_motion_primitive import PolynomialMotionPrimitive
 #from motion_primitives_py.msg import VoxelMap
 
@@ -12,14 +14,17 @@ class OccupancyMap():
         msgs = [msg for _, msg, _ in bag.read_messages(topics=['/voxel_map'])]
         bag.close()
         
-        # only save the first VoxelMap message
-        self.voxels = np.array(msgs[0].data)
+        # save the first VoxelMap message
         self.resolution = msgs[0].resolution
         self.dims = np.array([msgs[0].dim.x, msgs[0].dim.y, msgs[0].dim.z]).astype(int)
         self.origin = np.array([msgs[0].origin.x, msgs[0].origin.y, msgs[0].origin.z])
         if self.dims[2] == 1:
             self.dims = self.dims[:2]
             self.origin = self.origin[:2]
+            self.is_free = self._is_free_2D
+        else:
+            self.is_free = self._is_free_3D
+        self.voxels = np.array(msgs[0].data).reshape(self.dims, order='F')
 
         # distance margin to inflate obstacles by
         self.margin = margin
@@ -30,24 +35,22 @@ class OccupancyMap():
     def get_voxel_center(self, indices):
         return self.resolution * (indices + .5) + self.origin
 
-    def in_bounds(self, indices):    
+    def is_in_bounds(self, indices):    
         if any(indices < 0) or any((self.dims - indices) < 0):
             return False
         else:
             return True
-
-    def is_free(self, indices):
-        if self.voxels[np.ravel_multi_index(tuple(indices), tuple(self.dims), order='F')] == 0:
-            return True
-        else: 
+    
+    def is_valid_position(self, point):
+        min = self.get_indices(point - self.margin)
+        max = self.get_indices(point + self.margin)
+        if not self.is_in_bounds(min) or not self.is_in_bounds(max) \
+                                      or not self.is_free(min, max):
             return False
+        else:
+            return True
 
-    def plot(self):
-        if len(self.dims) == 2:
-            plt.imshow(self.voxels.reshape(self.dims, order='F').T, 
-                       cmap=plt.cm.gray_r)
-
-    def collision(self, mp, offset=np.array([0, 0, 0])):
+    def mp_has_collision(self, mp, offset=np.array([0, 0, 0]), plot=False):
         """
         Function to check if there is a collision between a motion primitive
         trajectory and the occupancy map
@@ -62,12 +65,38 @@ class OccupancyMap():
         # TODO make number of points a parameter to pass in here
         _, samples, _, _, _, = mp.get_sampled_states()
         for sample in samples.T + offset[:len(self.dims)]:
-            indices = self.get_indices(sample)
-            if not self.in_bounds(indices) or not self.is_free(indices):
-                plt.plot(indices[0], indices[1], '.r')
+            # TODO this is inefficient take out plotting stuff after its tested
+            if plot:
+                indices = self.get_indices(sample)
+            if not self.is_valid_position(sample):
+                if plot:
+                    plt.plot(indices[0], indices[1], 'or')
+                    self.get_voxel_center(indices)
                 return True
-            plt.plot(indices[0], indices[1], '.g')
+            if plot:
+                plt.plot(indices[0], indices[1], 'og')
         return False
+
+    def plot(self):
+        if len(self.dims) == 2:
+            plt.imshow(self.voxels.T, cmap=plt.cm.gray_r)
+
+    # ----------------------  PRIVATE FUNCTIONS ----------------------------- 
+
+    def _is_free_2D(self, min_indices, max_indices):
+        if (self.voxels[min_indices[0]:max_indices[0], 
+                        min_indices[1]:max_indices[1]] == 0).all():
+            return True
+        else: 
+            return False
+
+    def _is_free_3D(self, min_indices, max_indices):
+        if (self.voxels[min_indices[0]:max_indices[0], 
+                        min_indices[1]:max_indices[1],
+                        min_indices[2]:max_indices[2]] == 0).all():
+            return True
+        else: 
+            return False
 
 if __name__ == "__main__":
     # problem parameters
@@ -75,14 +104,14 @@ if __name__ == "__main__":
     control_space_q = 3
     
     # setup occupancy map
-    occ_map = OccupancyMap('trees_dispersion_0.6_1.bag', 0)
+    occ_map = OccupancyMap('trees_dispersion_0.6_1.bag', 1)
     occ_map.plot()
 
     # setup sample motion primitive
-    start_position = occ_map.get_voxel_center(np.array([350, 100]))
+    start_position = occ_map.get_voxel_center(np.array([300, 100]))
     start_state = np.zeros((num_dims * control_space_q,))
     start_state[:num_dims] = start_position[:num_dims]
-    end_position = occ_map.get_voxel_center(np.array([450, 150]))
+    end_position = occ_map.get_voxel_center(np.array([400, 150]))
     end_state = np.zeros((num_dims * control_space_q,))
     end_state[:num_dims] = end_position[:num_dims]
     max_state = 100 * np.ones((num_dims * control_space_q,))
@@ -90,6 +119,6 @@ if __name__ == "__main__":
 
     # check collision
     if mp.is_valid:
-        print(occ_map.collision(mp))
+        print(occ_map.mp_has_collision(mp, plot=True))
 
     plt.show()
