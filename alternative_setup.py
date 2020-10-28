7a737cf53010e1a4e04d9fc5a9f06809ff8ce321
laura <ljarinl@gmail.com>
2020-10-28 15:01:18 -0400

    fix setup.py (should install package with pip now, not ROS)

--- alternative_setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='motion_primitives_py', version='0.0.1', packages=['motion_primitives_py'], python_requires='>3.6',
-      install_requires=['ujson',
-                        'sympy',
-                        'py_opt_control @ git+ssh://git@github.com/jpaulos/opt_control#egg=py_opt_control&subdirectory=python',
-                        'reeds_shepp @ git+ssh://git@github.com/ghliu/pyReedsShepp#egg=reeds_shepp',
-                        ]
-      )
--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='motion_primitives_py', version='0.0.1', packages=['motion_primitives_py'])
+setup(name='motion_primitives_py', version='0.0.1', packages=['motion_primitives_py'], python_requires='>3.6',
+      install_requires=['ujson',
+                        'sympy',
+                        'py_opt_control @ git+ssh://git@github.com/jpaulos/opt_control#egg=py_opt_control&subdirectory=python',
+                        'reeds_shepp @ git+ssh://git@github.com/ghliu/pyReedsShepp#egg=reeds_shepp',
+                        'sobol_seq @ git+ssh://git@github.com/naught101/sobol_seq@v0.2.0#egg=sobol_seq'
+                        ]
+      )
