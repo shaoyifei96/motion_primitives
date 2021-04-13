534b352478abcf64f1d3dcfe6ed2984292f0179d
chao <quchao@seas.upenn.edu>
2021-04-13 00:08:07 -0400

    add some simple tests

---
+++ tests/test_graph_search.cpp
@@ -0,0 +1,13 @@
+
+#include <gtest/gtest.h>
+
+#include "motion_primitives/graph_search.h"
+
+namespace {
+
+TEST(GraphSearchTest, DummyTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(false);
+}
+
+}  // namespace
---
+++ tests/test_motion_primitive.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "motion_primitives/motion_primitive_graph.h"
+
+namespace {
+
+TEST(MotionPrimitiveTest, DummyTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+}  // namespace
